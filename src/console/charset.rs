a0466025449a60153193abcf89f923ed9588ba47
Laurent Pelecq <lpelecq+oprs@circoise.eu>
2022-09-25 09:59:47 +0200

    cleanup module console

--- src/console/charset.rs
+++ src/console/charset.rs
@@ -23,44 +23,6 @@ pub fn is_unicode() -> bool {
     }
 }
 
-/// Arrows in ASCII
-const ASCII_ARROWS_: [&str; 4] = ["<", "^", ">", "v"];
-
-/// Arrows in UTF8
-const UTF8_ARROWS_: [&str; 4] = ["←", "↑", "→", "↓"];
-
-/// Character types to draw arrows
-pub enum ArrowChar {
-    Left,
-    Up,
-    Right,
-    Down,
-}
-
-/// Characters to draw arrows
-pub struct ArrowCharSet(&'static [&'static str; 4]);
-
-impl ArrowCharSet {
-    pub fn new() -> ArrowCharSet {
-        ArrowCharSet(if is_unicode() {
-            &UTF8_ARROWS_
-        } else {
-            &ASCII_ARROWS_
-        })
-    }
-
-    /// Get a specific character to draw a table
-    pub fn get(&self, kind: ArrowChar) -> &'static str {
-        let ArrowCharSet(charset) = self;
-        charset[match kind {
-            ArrowChar::Left => 0usize,
-            ArrowChar::Up => 1usize,
-            ArrowChar::Right => 2usize,
-            ArrowChar::Down => 3usize,
-        }]
-    }
-}
-
 /// Characters to draw a table in ASCII
 const ASCII_TABLE_CHARS_: [&str; 13] = [
     "-", "|", "+", "+", "+", "+", "+", "+", "-", "-", "-", "|", "+",
@@ -71,13 +33,10 @@ const UTF8_TABLE_CHARS_: [&str; 13] = [
     "─", "│", "┌", "┐", "└", "┘", "├", "┤", "┬", "┴", "─", "│", "┼",
 ];
 
-/// Characters to draw a table without lines
-const BLANK_TABLE_CHARS_: [&str; 13] = ["", "", "", "", "", "", "", "", "", "", " ", " ", " "];
-
 /// Character types to draw a table
 pub enum TableChar {
     Horizontal,
-    Vertical,
+    _Vertical,
     DownRight,
     DownLeft,
     UpRight,
@@ -109,18 +68,11 @@ impl TableCharSet {
         }
     }
 
-    pub fn without_lines() -> TableCharSet {
-        TableCharSet {
-            chars: &BLANK_TABLE_CHARS_,
-            border_width: 0,
-        }
-    }
-
     /// Get a specific character to draw a table
     pub fn get(&self, kind: TableChar) -> &'static str {
         self.chars[match kind {
             TableChar::Horizontal => 0usize,
-            TableChar::Vertical => 1usize,
+            TableChar::_Vertical => 1usize,
             TableChar::DownRight => 2usize,
             TableChar::DownLeft => 3usize,
             TableChar::UpRight => 4usize,
@@ -144,10 +96,5 @@ impl TableCharSet {
     /// An inner horizontal line of a given size.
     pub fn inner_horizontal_line(&self, count: usize) -> (usize, String) {
         self.repeat(TableChar::HorizontalInner, count)
-    }
-
-    /// A border horizontal line of a given size.
-    pub fn outter_horizontal_line(&self, count: usize) -> (usize, String) {
-        self.repeat(TableChar::Horizontal, count)
     }
 }
